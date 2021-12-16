2810a365ac1304e94297bf2a024c7fc3660184f0
Nicolau Barsotti <nicolau.barsotti@flexcodellc.com>
2021-12-16 20:26:06 -0300

    feat(weathercontainer): add data parser, create default structure for all cases

--- src/Containers/WeatherContainer.tsx
+++ src/Containers/WeatherContainer.tsx
@@ -2,21 +2,70 @@ import { useState, useEffect } from "react";
 import useRequest from "../hooks/useRequest";
 import { getWeather } from '../services/endpoints';
 import usePosition from '../contexts/PositionContext';
+import WeatherCard, { Display, WeatherData, WeatherProps } from "../components/weatherCard/WeatherCard";
+
+interface APIResponse { 
+  weather: Record<string, unknown>[];
+  main: Record<string, unknown>;
+  wind: Record<string, unknown>;
+}
+
+const weatherDataParser = (weatherData: Record<string, unknown>): WeatherProps => {
+  const { weather, main, wind } = weatherData as unknown as APIResponse;
+  const weatherWithin = weather[0];
+
+  const data = {
+    temp: main.temp,
+    tempFeel: main.feels_like,
+    pressure: main.pressure,
+    humidity: main.humidity,
+    speed: wind.speed,
+    direction: wind.deg
+  } as WeatherData;
+
+  const display = { 
+    main: weatherWithin.main,
+    description: weatherWithin.description,
+    icon: weatherWithin.icon,
+    city: 'Santos' // WIP!!!
+  } as Display;
+
+  return {
+    display,
+    data
+  };
+};
 
 export default (): JSX.Element => { 
   const [params, setParams] = useState({});
-  const [weather, setWeather] = useState<Record<string, unknown>>();
-  const { lat, lon } = usePosition();
+  const [weather, setWeather] = useState<WeatherProps | null>(null);
+  const [waitUser, setWaitUser] = useState(true);
+  const { lat, lon, error: browserGeoError } = usePosition();
 
   const { data, error, loading } = useRequest(params);
 
   useEffect(() => { 
     if (lat !== 0 || lon !== 0) { 
       setParams(getWeather(lat, lon));
+      setWaitUser(false);
     }
   }, [lat, lon]);
 
+  useEffect(() => {
+    if (data) setWeather(weatherDataParser(data));
+  }, [data]);
+
+  useEffect(() => {
+    if (browserGeoError || error) setWaitUser(false);
+  }, [browserGeoError, error]);
+
   return (
-    <div>Hi</div>
+    <>
+      {waitUser && <p>Waiting on user input</p>}
+      {browserGeoError && <p>User failed to provide their geolocation data</p>}
+      {loading && <p>Fetching data...</p>}
+      {error && <p>Network Error: {error}</p>}
+      {weather && <WeatherCard {...weather} />} 
+    </>
   );
 };
