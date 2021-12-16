dc35b2b5c0ca3641abf5e7447a728466844c7b56
Nicolau Barsotti <nicolau.barsotti@flexcodellc.com>
2021-12-16 15:58:44 -0300

    feat: import appid and some extra default config for the endpoints

--- src/services/endpoints.ts
+++ src/services/endpoints.ts
@@ -1,4 +1,12 @@
+const appid = process.env.REACT_API_KEY;
+
+const baseParams = {
+  units: 'metric',
+  appid
+};
+
 export const getWeather = (lat: number, lon: number) => ({
   lat,
-  lon
+  lon,
+  ...baseParams
 });
