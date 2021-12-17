901081624a147dae14cb07ac135049b4ad9a1e24
Nicolau Barsotti <nicolau.barsotti@flexcodellc.com>
2021-12-17 14:49:50 -0300

    feat: add logic to fetch actual city name

--- src/Containers/WeatherContainer.tsx
+++ src/Containers/WeatherContainer.tsx
@@ -1,20 +1,24 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import useRequest from "../hooks/useRequest";
-import { getWeather } from '../services/endpoints';
+import { getWeather, getCityName } from '../services/endpoints';
 import usePosition from '../contexts/PositionContext';
 import WeatherCard, { Display, WeatherData, WeatherProps } from "../components/weatherCard/WeatherCard";
 import ErrorCard, { ErrorType } from '../components/UI/errorCard/ErrorCard';
 import Loading, { LoadingType } from '../components/UI/loading/Loading';
 import { WeatherContainer } from './styles';
 
-interface APIResponse { 
+interface WeatherAPIResponse { 
   weather: Record<string, unknown>[];
   main: Record<string, unknown>;
   wind: Record<string, unknown>;
 }
 
+interface ReverseGeoLocationResponse { 
+  name: string;
+}
+
 const weatherDataParser = (weatherData: Record<string, unknown>): WeatherProps => {
-  const { weather, main, wind } = weatherData as unknown as APIResponse;
+  const { weather, main, wind } = weatherData as unknown as WeatherAPIResponse;
   const weatherWithin = weather[0];
 
   const data = {
@@ -30,7 +34,7 @@ const weatherDataParser = (weatherData: Record<string, unknown>): WeatherProps =
     main: weatherWithin.main,
     description: weatherWithin.description,
     icon: weatherWithin.icon,
-    city: 'Santos' // WIP!!!
+    city: ''
   } as Display;
 
   return {
@@ -39,17 +43,31 @@ const weatherDataParser = (weatherData: Record<string, unknown>): WeatherProps =
   };
 };
 
+const injectCityName = (name: string, weatherObj: WeatherProps): WeatherProps => {
+  return {
+    ...weatherObj,
+    display: { 
+      ...weatherObj.display,
+      city: name
+    }
+  };
+};
+
 export default (): JSX.Element => { 
   const [params, setParams] = useState({});
-  const [weather, setWeather] = useState<WeatherProps | null>(null);
+  const [cityParams, setCityParams] = useState({});
+  const [weather, setWeather] = useState<WeatherProps>();
+  const [city, setCity] = useState<string>('');
   const [waitUser, setWaitUser] = useState(true);
   const { lat, lon, error: browserGeoError } = usePosition();
 
   const { data, error, loading } = useRequest(params);
+  const { data: cityData } = useRequest(cityParams);
 
   useEffect(() => { 
     if (lat !== 0 || lon !== 0) { 
       setParams(getWeather(lat, lon));
+      setCityParams(getCityName(lat, lon));
       setWaitUser(false);
     }
   }, [lat, lon]);
@@ -57,6 +75,17 @@ export default (): JSX.Element => {
   useEffect(() => {
     if (data) setWeather(weatherDataParser(data));
   }, [data]);
+
+  useEffect(() => {
+    if (cityData) {
+      const data = cityData[0] as unknown as ReverseGeoLocationResponse;
+      setCity(data.name);
+    }
+  }, [cityData]);
+
+  useEffect(() => {
+    if (city) setWeather(w => w && injectCityName(city, w));
+  }, [city]);
 
   useEffect(() => {
     if (browserGeoError || error) setWaitUser(false);
