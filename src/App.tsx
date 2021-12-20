fe72bae049947cb33ec07ab4df243b6b18ae1b97
Nicolau Barsotti <nicolau.barsotti@flexcodellc.com>
2021-12-19 21:21:15 -0300

    feat: integrate WeatherContainer into App (render outside storybook)

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
+import WeatherContainer from './Containers/WeatherContainer';
+import { PositionProvider } from "./contexts/PositionContext";
 
 export default (): JSX.Element => { 
   return (
-    <h1>Builders</h1>
+    <PositionProvider>
+      <WeatherContainer />
+    </PositionProvider>
   );
 };
